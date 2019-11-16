8e809cd91c6cbb2f5576932f5ab862e094e252af
abraham <abrahamtoledo90@gmail.com>
2019-11-16 15:32:19 -0500

    - Refactored using OOP - CONFIG_DIR can be set in NAUTA_CONFIG_DIR Environment variable

---
+++ test/nauta.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestNautaMethods(unittest.TestCase):
+    def setUp(self):
+
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_expand_username(self):
+        pass
+
